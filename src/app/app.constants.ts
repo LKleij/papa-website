08ed1de8df381a7f53ee3167eed74a5aa8834f29
Lucas <kleijvander@gmail.com>
2021-12-05 00:46:57 +0100

    hel

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,7 @@
 import { BehaviorSubject, Observable } from "rxjs";
 
 export const initialViewportSize = { width: window.innerWidth * window.devicePixelRatio, height: window.innerHeight * window.devicePixelRatio };
-export const initialNavHeight = 100;
-export const devicePixelRatio = new BehaviorSubject<number>(null!).next(window.devicePixelRatio);
+export const initialNavHeight = 70;
 
 
-export const globalPaddingLeftAndRight = '10%'
+export const globalPaddingLeftAndRight = '0px'
